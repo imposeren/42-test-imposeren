77b40c080f6e25d82efcc3bdc5d4f090a944bfc4
Yaroslav Klyuyev <imposeren@gmail.com>
2011-12-20 23:44:13 +0200

    ticket:5 restricted edition, request to superuser

--- testsite/mylogging/views.py
+++ testsite/mylogging/views.py
@@ -2,10 +2,12 @@
 from django.views.generic import ListView
 from testsite.mylogging.models import Request
 
+from testsite.decorators import superuser_only
 
+@superuser_only
 class IndexView(ListView):
     queryset = Request.objects.order_by('-date')[:10]
 
-
+@superuser_only
 def index(request):
     return IndexView.as_view()(request)
--- testsite/profiles/views.py
+++ testsite/profiles/views.py
@@ -11,5 +11,24 @@ def index(request):
     """Show detail on me"""
     return IndexView.as_view()(request, pk=1)
     
+from django.forms.models import modelformset_factory
+from django.template import RequestContext
+from django.shortcuts import render_to_response, redirect
+from testsite.decorators import superuser_only
+
+@superuser_only
 def edit(request, pk=1):
-    return IndexView.as_view()(request, pk=1)
+    ProfileFormSet = modelformset_factory(Profile, max_num=1)
+
+    if request.method == 'POST':
+        formset = ProfileFormSet(request.POST, request.FILES,
+                                 queryset=Profile.objects.filter(pk=1))
+        if formset.is_valid():
+            formset.save()
+        return redirect(index)
+    else:
+        formset = ProfileFormSet(queryset=Profile.objects.filter(pk=1))
+
+    return render_to_response('profiles/edit.html', {'formset':formset},
+                              context_instance = RequestContext(request))
+ 

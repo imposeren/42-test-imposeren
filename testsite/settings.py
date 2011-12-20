755ce75a809b6023fa426152a538af4ea9febfe2
Yaroslav Klyuyev <imposeren@gmail.com>
2011-12-20 16:01:09 +0200

    ticket:3 settings for static path fixed

--- testsite/settings.py
+++ testsite/settings.py
@@ -74,7 +74,6 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, 'templates/static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.

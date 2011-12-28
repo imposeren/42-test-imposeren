9886c0d11fdabb9bd4c85673ec2e6915f3a93626
Yaroslav Klyuyev <imposeren@gmail.com>
2011-12-28 22:52:34 +0200

    ticket:8 login sequence changed

--- testsite/profiles/windmilltests/testforms.py
+++ testsite/profiles/windmilltests/testforms.py
@@ -5,8 +5,7 @@ from windmill.authoring import WindmillTestClient
 
 def test_ajax_forms():
     client = WindmillTestClient(__name__)
-    client.click(link=u'Edit')
-    client.waits.forPageLoad(timeout=u'20000')
+
     client.waits.forElement(link=u'Login', timeout=u'8000')
     client.click(link=u'Login')
     client.click(id=u'id_username')
@@ -15,6 +14,10 @@ def test_ajax_forms():
     client.click(value=u'login')
     client.waits.forPageLoad(timeout=u'20000')
 
+    client.click(link=u'Edit')
+    client.waits.forPageLoad(timeout=u'20000')
+
+    # change birthdate
     client.waits.forElement(timeout=u'8000', id=u'id_birth')
     client.click(id=u'id_birth')
     client.waits.sleep(milliseconds=1000)
@@ -25,11 +28,10 @@ def test_ajax_forms():
     client.select(classname=u'ui-datepicker-year', val=u'1986')
     client.click(link=u'10')
     client.click(name=u'save')
-
-    #And this should work anyway:
     client.asserts.assertText(id="message", validator="Done. Reloading...")
     client.waits.forPageLoad(timeout=u'20000')
 
+    # test wrong date
     client.click(timeout=u'8000', id=u'id_birth')
     client.type(text=u'1987-12-1122', id=u'id_birth')
     client.click(name=u'save')

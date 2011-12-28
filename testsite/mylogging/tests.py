76bc0a14e1300878d2ff1eb211cb9207f9583532
Yaroslav Klyuyev <imposeren@gmail.com>
2011-12-28 21:00:03 +0200

    ticket:8 initial tests

--- testsite/mylogging/tests.py
+++ testsite/mylogging/tests.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from tddspry.django import HttpTestCase
+from tddspry.django import TestCase
 from testsite.mylogging.models import Request
+from testsite.mylogging.templatetags.mylogging_tags import edit_link
+from django.test.client import RequestFactory
+from django.template import RequestContext
+from django.template import Template
+from django import template
 
 
 class TestLogger(HttpTestCase):
@@ -16,3 +22,20 @@ class TestLogger(HttpTestCase):
         self.go200('/requests/')
         self.find(r"\bGET(\s)+/requests/ ")
         self.find(r"\bGET(\s)+/")
+
+
+class testTag(TestCase):
+    def setUp(self):
+        register = template.Library()
+        template.libraries['django.templatetags.mylogging_tags'] = register
+        register.tag('edit_link ', edit_link)
+
+    def test_tag(self):
+        factory = RequestFactory()
+        request = factory.get('/')
+        context = RequestContext(request, {'foo': 'bar'})
+        out = Template(
+            "{% load mylogging_tags %}"
+            "{% edit_link request.user %}"
+        ).render(context)
+        self.asserEquals(rendered, "/admin/auth/user/1/")

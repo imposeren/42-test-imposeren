906248f4f2871cf7102e8e0c39b86eeb6e78094f
Yaroslav Klyuyev <imposeren@gmail.com>
2011-12-30 12:31:40 +0200

    ticket:10 model to store logs

--- testsite/mylogging/admin.py
+++ testsite/mylogging/admin.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from testsite.mylogging.models import Request
+from testsite.mylogging.models import Request, Modellog
 from django.contrib import admin
 
 admin.site.register(Request)
+admin.site.register(Modellog)
--- testsite/mylogging/models.py
+++ testsite/mylogging/models.py
@@ -12,3 +12,21 @@ class Request(models.Model):
 
     def __unicode__(self):
         return "%s: %s %s " % (self.date, self.method, self.path)
+
+
+class Modellog(models.Model):
+    """Log entry about database modification"""
+    ACTION_CHOICES = (
+        ('C', 'create'),
+        ('E', 'edit'),
+        ('D', 'delete'),
+    )
+    action = models.CharField(max_length=1, choices=ACTION_CHOICES)
+    inst_pk = models.IntegerField()
+    app = models.CharField(max_length=32, choices=ACTION_CHOICES)
+    model = models.CharField(max_length=32, choices=ACTION_CHOICES)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "%s: %s %s.%s, pk=%s " % (self.date, self.action, self.app,
+                                         self.model, self.inst_pk)

67743613fd204394b00b4085a6a4319eb34f64cf
Yaroslav Klyuyev <imposeren@gmail.com>
2012-01-04 15:52:57 +0200

    ticket:15 done?

--- testsite/profiles/forms.py
+++ testsite/profiles/forms.py
@@ -3,6 +3,9 @@ from django import forms
 from testsite.profiles.models import Contact, Profile
 from django.forms.models import inlineformset_factory
 from django.forms.models import BaseModelFormSet
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+import re
 
 
 def readonly(form_or_set):
@@ -33,6 +36,9 @@ class ProfileForm(forms.ModelForm):
         }
 
 
+PHONE_REGEXP = r"^(\+|0)?([0-9]{3})?[0-9]{9}$"
+
+
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
@@ -45,9 +51,27 @@ class ContactForm(forms.ModelForm):
         cleaned_data = self.cleaned_data
         mean = cleaned_data.get("mean")
         data = cleaned_data.get("data")
-        if mean in ['email', 'e-mail']:
-            pass
 
+        if re.match('e?mail', mean):
+            try:
+                cleaned_data["mean"] = "email"
+                validate_email(data)
+            except ValidationError, msgs:
+                message = "Enter a valid e-mail address."
+                self._errors["data"] = self.error_class([message])
+                del cleaned_data["data"]
+
+        elif re.match('phone.*', mean):
+            for ignoreit in [' ', '(', ')']:
+                data = data.replace(ignoreit, '')
+            if not re.match(PHONE_REGEXP, data):
+                message = "Enter a valid phone number."
+                self._errors["data"] = self.error_class([message])
+                del cleaned_data["data"]
+            else:
+                cleaned_data["data"] = data
+
+        return cleaned_data
 
 
 ContactFormSet = inlineformset_factory(Profile, Contact,
